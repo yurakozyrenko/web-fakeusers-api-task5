41c03584c93b004912ec1d3f7eb5e29b8d3434ff
Yura <96086557+yurakozyrenko@users.noreply.github.com>
2024-02-21 15:17:26 +0300

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 require('dotenv').config();
-const app = express();
 const cors = require('cors');
+const app = express();
 const PORT = process.env.PORT ?? 4000;
 
 app.use(express.json());
